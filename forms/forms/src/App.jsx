c8345001be4de64580500d76ac6455e09167be6f
THOMAS SANTOS DE SOUZA <thomas.ssouza1@senacsp.edu.br>
2026-05-12 11:55:32 -0300

    forms

---
+++ forms/forms/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import MeuFormulario from './components/MeuFormulario';
+
+function App() {
+
+  return (
+   <div className="App">
+    <h1>Formulário com React</h1>
+    <MeuFormulario />
+   </div>
+  );
+};
+export default App;
---
+++ forms/forms/src/components/MeuFormulario.jsx
@@ -0,0 +1,76 @@
+import "./MeuFormulario.css";
+
+import { useState } from "react";
+
+const MeuFormulario = () => {
+    // gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [biografia, setBiografia] = useState("");
+    const [role, setRole] = useState("");
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+
+    console.log(name, email);
+
+    //Envio de form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        console.log(name, email, biografia, role);
+
+        //Limpar formulário
+    setName("");
+    setEmail("");
+    setBiografia("");
+    setRole("");
+    }
+
+
+    
+  return (
+    <div>
+        {/* Criação do form */}
+        {/* Envio de Formulário */}
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name="name" placeholder="Digite o seu nome" 
+                onChange={handleName} value={name || ""} />
+            </div>
+
+            {/* label envolvendo input */}
+            <label>
+                <span>E-mail:</span>
+                <input text="text" name="email" placeholder="Digite o seu e-mail" 
+                onChange={(e) => setEmail(e.target.value)} value={email || ""} />
+            </label>
+
+            {/* textarea */}
+            <label>
+                <span>Biografia:</span>
+                <textarea name="biografia" placeholder="Descrição do usuário"
+                onChange={(e) => setBiografia(e.target.value)} value={biografia || ""}>
+                </textarea>
+            </label>
+
+            {/* Select */}
+            <label>
+                <span>Função no sistema</span>
+                <select name="role" onChange={(e) => setRole(e.target.value)}
+                value={role || ""}>
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="admin">Admin</option>
+                </select>
+            </label>
+
+            <input type="submit" value="Enviar" />
+        </form>
+    </div>
+  )
+}
+
+export default MeuFormulario;
